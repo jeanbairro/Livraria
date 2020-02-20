6f49d7260a3b884bd02d2f9231cd1ffcc9817556
Jean Bairro <jeancasulo@gmail.com>
2020-02-20 20:55:25 -0300

    Criação e configuração dos arquivos do banco de dados.

---
+++ Livraria/5 - Infra/5.2 - Database/ClassLibrary1/Configs/LivroConfig.cs
@@ -0,0 +1,21 @@
+﻿using Livraria.Domain.Livros;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Livraria.Database.Configs
+{
+    public class LivroConfig : IEntityTypeConfiguration<Livro>
+    {
+        public void Configure(EntityTypeBuilder<Livro> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable(nameof(Livro));
+
+            builder.Property(x => x.Autor).HasMaxLength(Livro.AutorMaxLength).IsRequired();
+            builder.Property(x => x.Editora).HasMaxLength(Livro.EditoraMaxLength).IsRequired();
+            builder.Property(x => x.Titulo).HasMaxLength(Livro.TituloMaxLength).IsRequired();
+
+            builder.HasIndex(x => x.Id);
+        }
+    }
+}
---
+++ Livraria/5 - Infra/5.2 - Database/ClassLibrary1/Contexts/ILivrariaContext.cs
@@ -0,0 +1,12 @@
+﻿using Livraria.Domain.Livros;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livraria.Database.Contexts
+{
+    public interface ILivrariaContext
+    {
+        DbSet<Livro> Livros { get; set; }
+
+        bool AllMigrationsApplied();
+    }
+}
