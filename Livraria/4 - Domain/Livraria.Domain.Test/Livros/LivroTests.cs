e3f8cb994d337025bcdac8f2301a52668529157c
Jean Bairro <jeancasulo@gmail.com>
2020-02-23 09:47:32 -0300

    Melhorando os testes.

--- Livraria/4 - Domain/Livraria.Domain.Test/Livros/LivroTests.cs
+++ Livraria/4 - Domain/Livraria.Domain.Test/Livros/LivroTests.cs
@@ -1,39 +1,34 @@
-﻿using Livraria.Domain.Livros;
-using Xunit;
+﻿using Xunit;
 
 namespace Livraria.Domain.Test.Livros
 {
+    [Collection(nameof(LivroCollection))]
     public class LivroTests
     {
-        [Fact(DisplayName = "Cria um livro inválido pelo builder")]
+        public LivroTests(LivroTestsFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        public LivroTestsFixture Fixture { get; set; }
+
+        [Fact(DisplayName = "Cria um livro inválido")]
         public void CriarUmLivroInvalido()
         {
-            var builder = GetBuilder();
-            var livro = builder.Build();
+            var livro = Fixture.GetLivroInvalido();
 
             Assert.NotNull(livro);
             Assert.False(livro.IsValid());
             Assert.NotEqual(0, livro.Errors.Count);
         }
 
-        [Fact(DisplayName = "Cria um livro válido pelo builder")]
+        [Fact(DisplayName = "Cria um livro válido")]
         public void CriarUmLivroValido()
         {
-            var builder = GetBuilder();
-            var livro = builder
-                .WithAutor("J. K. Rowling")
-                .WithEditora("Rocco")
-                .WithTitulo("Harry potter e a pedra filosofal")
-                .Build();
-
+            var livro = Fixture.GetLivroValido();
             Assert.NotNull(livro);
             Assert.Empty(livro.Errors);
-            Assert.Equal("J. K. Rowling", livro.Autor);
-            Assert.Equal("Rocco", livro.Editora);
-            Assert.Equal("Harry potter e a pedra filosofal", livro.Titulo);
             Assert.True(livro.IsValid());
         }
-
-        private LivroBuilder GetBuilder() => new LivroBuilder();
     }
 }
